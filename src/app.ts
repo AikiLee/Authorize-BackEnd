15d7b47f8953ce664c755c9e7c8af1562ac66c0b
leeburn <leeburniex@outlook.com>
2025-08-15 07:17:21 +0800

    discard app.listen and export app for vercel to use

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -23,19 +23,18 @@ const PORT = process.env.PORT || 3001;
 
 // 配置 CORS 选项
 const corsOptions = {
-  origin: '*', // 前端应用的域
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   credentials: true, // 是否允许发送凭证（Cookies）
+    origin: "*", // 前端应用的域
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    //   credentials: true, // 是否允许发送凭证（Cookies）
 };
-
 
 // 使用 morgan 记录请求日志
 app.use(
-  morgan("combined", {
-    stream: {
-      write: (message: string) => logger.info(message.trim()), // 将请求日志写入 winston
-    },
-  })
+    morgan("combined", {
+        stream: {
+            write: (message: string) => logger.info(message.trim()), // 将请求日志写入 winston
+        },
+    })
 );
 
 // 使用 CORS 中间件
@@ -52,13 +51,16 @@ app.use(errorHandler); // 错误处理中间件
 app.use("/api", routes);
 
 sequelize
-  .sync({ force: false }) // 设置为 true 将会删除并重建表，生产环境请设为 false
-  .then(() => {
-    console.log("数据库同步成功");
-    app.listen(PORT, () => {
-      console.log(`服务器运行在端口 ${PORT}`);
+    .sync({ force: false }) // 设置为 true 将会删除并重建表，生产环境请设为 false
+    .then(() => {
+        console.log("数据库同步成功");
+        // app.listen(PORT, () => {
+        //     console.log(`服务器运行在端口 ${PORT}`);
+        // });
+    })
+    .catch((err) => {
+        console.error("数据库同步失败:", err);
     });
-  })
-  .catch((err) => {
-    console.error("数据库同步失败:", err);
-  });
+
+// 导出app实例以供vercel调用 
+export default app;
