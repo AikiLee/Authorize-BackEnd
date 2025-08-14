c80edb966c4e632597ee1d067ddd7d208a0f11b7
leeburn <leeburniex@outlook.com>
2025-08-15 07:56:04 +0800

    change2

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,8 +6,9 @@ const database = process.env.DB_NAME!;
 const username = process.env.DB_USER!;
 const password = process.env.DB_PASSWORD!;
 const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
 
-console.log(database, username, password, host);
+console.log(database, username, password, host, port);
 /* 
     通过.env获取变量设置
     process可以直接使用
@@ -18,8 +19,11 @@ const sequelize = new Sequelize(
     database, 
     username, 
     password, 
+
     {
         host,
+        port: Number(port),
+
         dialect: "mysql",
         // 添加调试日志用于验证配置
         logging: console.log,
